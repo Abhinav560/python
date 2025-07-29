1b845c6747c22e9e6b3776341c67b48b268d8077
Abhinav560 <abhinavtayal5497.com>
2025-07-29 15:50:40 +0530

    1

--- exceptions.py
+++ exceptions.py
@@ -4,8 +4,16 @@
 # except ZeroDivisionError:
 #     print("can't be divided by zero.")
 
-try:
-    mylist=[1,2,3]
-    print(mylist[5])
-except IndexError:
-    print("Index out of range")
+# try:
+#     mylist=[1,2,3]
+#     print(mylist[5])
+# except IndexError:
+#     print("Index out of range")
+
+num=int(input("enter a number: "))
+if num>0: 
+    print("the number is positive")
+elif num==0:
+    print("number is zero")
+elif num<0:
+    print("the number is negative")
